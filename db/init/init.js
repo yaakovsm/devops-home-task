fe3630a8473e2eeab4e9ed2c1343f0db630ae37e
Your Name <yaakovsm@gmail.com>
2026-04-03 16:50:53 +0300

    MongoDB initialization script with seed data

---
+++ db/init/init.js
@@ -0,0 +1,10 @@
+db = db.getSiblingDB("fruitdb");
+
+db.fruits.drop();
+
+db.fruits.insertMany([
+  { _id: 1, name: "apples", qty: 5, rating: 3 },
+  { _id: 2, name: "bananas", qty: 7, rating: 1, microsieverts: 0.1 },
+  { _id: 3, name: "oranges", qty: 6, rating: 2 },
+  { _id: 4, name: "avocados", qty: 3, rating: 5 }
+]);
